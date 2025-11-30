71b279e4aa0f9f4cf5aecc5dc3af3665f150a9a7
modii1 <zerof443@gmail.com>
2025-11-30 16:49:03 +0300

    Remove comment from worker.js
    Removed comment in Arabic about serving files from the public folder.

--- worker.js
+++ worker.js
@@ -2,11 +2,10 @@ import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
 export default {
   async fetch(request) {
-    // يخدم ملفات مجلد public (الذي بداخله index.html)
     return serveDir(request, {
       fsRoot: "public",
       showDirListing: false,
       quiet: true,
     });
-  }
+  },
 };
